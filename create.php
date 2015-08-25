dadba9ec67afdddcbb726e8886b1ee429352c126
RocioJp <achi_rocio@hotmail.com>
2015-08-25 12:42:38 -0300

    Update create.php

--- create.php
+++ create.php
@@ -1,34 +1,54 @@
-<?php
-if($_POST){
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
  
-    // include database connection
-    include 'config/database.php';
- 
-    try{
+<body>
+    <div class="container">
      
-        // insert query
-        $query = "INSERT INTO potluck SET name=:name, food=:food, confirmed=:confirmed";
- 
-        // prepare query for execution
-        $stmt = $con->prepare($query);
- 
-        // bind the parameters
-        $stmt->bindParam(':name', $_POST['name']);
-        $stmt->bindParam(':food', $_POST['food']);
-        $stmt->bindParam(':confirmed', $_POST['confirmed']);
-                
-        // Execute the query
-        if($stmt->execute()){
-            echo "<div>Record was saved.</div>";
-        }else{
-            die('Unable to save record.');
-        }
-         
-    }
-     
-    // show error
-    catch(PDOException $exception){
-        die('ERROR: ' . $exception->getMessage());
-    }
-}
-?>
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Create a Customer</h3>
+                    </div>
+             
+                    <form class="form-horizontal" action="create.php" method="post">
+                      <div class="control-group <?php echo !empty($nameError)?'error':'';?>">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <input name="name" type="text"  placeholder="Name" value="">
+                            <?php if (!empty($nameError)): ?>
+                                <span class="help-inline"><?php echo $nameError;?></span>
+                            <?php endif; ?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($emailError)?'error':'';?>">
+                        <label class="control-label">Food</label>
+                        <div class="controls">
+                            <input name="email" type="text" placeholder="Food" value="">
+                            <?php if (!empty($emailError)): ?>
+                                <span class="help-inline"><?php echo $emailError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="control-group <?php echo !empty($mobileError)?'error':'';?>">
+                        <label class="control-label">Confirm (Y o N)</label>
+                        <div class="controls">
+                            <input name="Confirm (Y o N)" type="text"  placeholder="Confirm (Y o N)" value="">
+                            <?php if (!empty($mobileError)): ?>
+                                <span class="help-inline"><?php echo $mobileError;?></span>
+                            <?php endif;?>
+                        </div>
+                      </div>
+                      <div class="form-actions">
+                          <button type="submit" class="btn btn-success">Create</button>
+                          <a class="btn" href="index.php">Back</a>
+                        </div>
+                    </form>
+                </div>
+                 
+    </div> <!-- /container -->
+  </body>
+</html>
