0e344a3bd8400145a381daab937f842649f3719f
RocioJp <achi_rocio@hotmail.com>
2015-11-18 19:21:07 -0300

    Update database.php

--- database.php
+++ database.php
@@ -9,24 +9,24 @@ $password = $_SERVER["password"];
  function __construct() {
         die('Init function is not allowed');
     }
-     
+
     function connect()
     {
        // One connection through whole application
        if ( null == self::$cont )
-       {     
+       {
         try
         {
-          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."db_name=".self::$db_Name, self::$username, self::$password); 
+          self::$cont =  new PDO( "mysql:host=".self::$dbHost.";"."db_name=".self::$db_Name, self::$username, self::$password);
         }
         catch(PDOException $exception){
-			
-           echo "Connection error: " . $exception->getMessage(); 
+
+           echo "Connection error: " . $exception->getMessage();
         }
        }
        return self::$cont;
     }
-     
+
     function disconnect()
     {
         self::$cont = null;
