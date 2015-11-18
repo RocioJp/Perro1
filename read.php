e016ab09e0dff2c1900cd1927d3e9639de8652bc
RocioJp <achi_rocio@hotmail.com>
2015-11-18 19:29:46 -0300

    Update read.php

--- read.php
+++ read.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>PDO Read Records - code from codeofaninja.com</title>
-  
+
     </head>
 <body>
 <?php
@@ -11,7 +11,7 @@
     if ( !empty($_GET['id'])) {
         $id = $_REQUEST['id'];
     }
-     
+
     if ( null==$id ) {
         header("Location: index.php");
     } else {
@@ -24,7 +24,7 @@
         Database::disconnect();
     }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -32,15 +32,55 @@
     <link   href="css/bootstrap.min.css" rel="stylesheet">
     <script src="js/bootstrap.min.js"></script>
 </head>
- 
+
 <body>
     <div class="container">
-     
+
+                <div class="span10 offset1">
+                    <div class="row">
+                        <h3>Read a Customer</h3>
+       $q->execute(array($id));
+        $data = $q->fetch(PDO::FETCH_ASSOC);
+        Database::disconnect();
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+
                 <div class="span10 offset1">
                     <div class="row">
                         <h3>Read a Customer</h3>
                     </div>
-                     
+
+                    <div class="form-horizontal" >
+                      <div class="control-group">
+                        <label class="control-label">Name</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['name'];?>
+                            </label>
+                        </div>
+                      </div>
+                      <div class="control-group">
+                        <label class="control-label">Food</label>
+                        <div class="controls">
+                            <label class="checkbox">
+                                <?php echo $data['food'];?>
+                            </label>
+                        </div>
+                      </div>
+                      <div class="control-group">
+                        <label class="control-label">Confirmed</label>
+
                     <div class="form-horizontal" >
                       <div class="control-group">
                         <label class="control-label">Name</label>
@@ -69,25 +109,45 @@
                         <div class="form-actions">
                           <a class="btn" href="index.php">Back</a>
                        </div>
-                     
-                      
+
+
                     </div>
                 </div>
-                 
+
     </div> <!-- /container -->
   </body>
 </html>
 <script type='text/javascript'>
 function delete_user( id ){
-     
+
+    var answer = confirm('Are you sure?');
+                              <label class="checkbox">
+                                <?php echo $data['confirm'];?>
+                            </label>
+                        </div>
+                      </div>
+                        <div class="form-actions">
+                          <a class="btn" href="index.php">Back</a>
+                       </div>
+
+
+                    </div>
+                </div>
+
+    </div> <!-- /container -->
+  </body>
+</html>
+<script type='text/javascript'>
+function delete_user( id ){
+
     var answer = confirm('Are you sure?');
     if (answer){
-        // if user clicked ok, 
+        // if user clicked ok,
         // pass the id to delete.php and execute the delete query
         window.location = 'delete.php?id=' + id;
-    } 
+    }
 }
 </script>
- 
+
 </body>
 </html>
